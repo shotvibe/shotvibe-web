8edb72efb7459735a17548a07ee8182af35abaab
benny <benny@shotvibe.com>
2013-02-10 12:42:32 +0200

    In main urls.py set during development to serve the photo_buckets

--- shotvibe_site/urls.py
+++ shotvibe_site/urls.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 
 import frontend.urls
@@ -18,3 +20,10 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
+
+# During development, serve the photos from the photo buckets
+standard_photo_bucket_suffix = '{0}/{1}.jpg'
+if settings.LOCAL_PHOTO_BUCKET_URL_FORMAT_STR.endswith(standard_photo_bucket_suffix):
+    urlpatterns += static(
+            prefix = settings.LOCAL_PHOTO_BUCKET_URL_FORMAT_STR[:-len(standard_photo_bucket_suffix)],
+            document_root = settings.LOCAL_PHOTO_BUCKETS_BASE_PATH)
