5fbcacfb622cc1d59a431283737c71f651b14aee
benny <benny@shotvibe.com>
2013-05-25 01:21:39 +0300

    mobile invite_page: Remove no longer needed debug info from page

--- frontend/mobile_views.py
+++ frontend/mobile_views.py
@@ -7,9 +7,7 @@ def invite_page(request, invite_code):
     test_user = { 'get_full_name': 'Benny' }
     test_album = Album.objects.get(pk=5)
 
-    ua = request.META.get('HTTP_USER_AGENT', '').lower()
-
-    device = get_device(ua)
+    device = get_device(request.META.get('HTTP_USER_AGENT', '').lower())
 
     if device == 'android':
         test_app_url = 'market://details?id=com.urbandictionary.android'
@@ -22,8 +20,7 @@ def invite_page(request, invite_code):
             'inviting_user' : test_user,
             'album' : test_album,
             'app_url' : test_app_url,
-            'device' : device,
-            'full_user_agent' : ua }
+            'device' : device }
     return render_to_response('frontend/mobile/invite_page.html', data, context_instance=RequestContext(request))
 
 def get_device(ua):
