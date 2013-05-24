6be12f892a56ef5658e5e4419f24a1a9c41279d6
benny <benny@shotvibe.com>
2013-05-25 01:19:55 +0300

    mobile invite_page: Detect android/iphone and show the correct badge

--- frontend/mobile_views.py
+++ frontend/mobile_views.py
@@ -8,11 +8,29 @@ def invite_page(request, invite_code):
     test_album = Album.objects.get(pk=5)
 
     ua = request.META.get('HTTP_USER_AGENT', '').lower()
-    test_app_url = 'market://details?id=com.urbandictionary.android'
+
+    device = get_device(ua)
+
+    if device == 'android':
+        test_app_url = 'market://details?id=com.urbandictionary.android'
+    elif device == 'iphone':
+        test_app_url = 'itms-apps://itunes.apple.com/us/app/urban-dictionary/id584986228'
+    else:
+        test_app_url = None
 
     data = {
             'inviting_user' : test_user,
             'album' : test_album,
             'app_url' : test_app_url,
+            'device' : device,
             'full_user_agent' : ua }
     return render_to_response('frontend/mobile/invite_page.html', data, context_instance=RequestContext(request))
+
+def get_device(ua):
+    if ua.find('iphone') > 0:
+        return 'iphone'
+
+    if ua.find('android') > 0:
+        return 'android'
+
+    return 'other'
