41eb2c5997d40ceb5cdbfa23e89762feb5220358
Vladimir Prudnikov <v.prudnikov@gmail.com>
2013-07-15 02:46:43 +0200

    Add migration dependency.

--- photos/migrations/0001_initial.py
+++ photos/migrations/0001_initial.py
@@ -7,6 +7,10 @@ from django.db import models
 
 class Migration(SchemaMigration):
 
+    depends_on = (
+        ("phone_auth", "0001_initial"),
+    )
+
     def forwards(self, orm):
         # Adding model 'Album'
         db.create_table(u'photos_album', (
