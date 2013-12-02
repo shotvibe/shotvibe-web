23efa9253c6d8ca75c34dee91b38c64e24e9021b
benny <benny@shotvibe.com>
2013-12-02 12:46:52 +0000

    Update Django to version 1.6 and tweak fixtures for compatibility

--- photos/tests.py
+++ photos/tests.py
@@ -25,7 +25,7 @@ def read_in_chunks(file_object, chunk_size=1024):
 
 @override_settings(LOCAL_PHOTO_BUCKETS_BASE_PATH='.tmp_photo_buckets')
 class ModelTest(TestCase):
-    fixtures = ['tests/test_users']
+    fixtures = ['test_users']
 
     def setUp(self):
         self.amanda = auth.get_user_model().objects.get(pk=2)
