db26a4ea903212bcba41f33b410c6a36065da9aa
benny <benny@shotvibe.com>
2014-03-29 15:48:19 +0300

    phone_auth.admin.AlbumMemberInline: Make album and added_by_user links

--- phone_auth/admin.py
+++ phone_auth/admin.py
@@ -93,8 +93,8 @@ class AlbumMemberInline(admin.TabularInline):
     verbose_name = 'Album'
     verbose_name_plural = 'Albums'
 
-    fields = ('album', 'added_by_user', 'datetime_added', 'last_access')
-    readonly_fields = ('album', 'added_by_user', 'datetime_added', 'last_access')
+    fields = ('album_link', 'added_by_user_link', 'datetime_added', 'last_access')
+    readonly_fields = fields
 
     ordering = ('datetime_added',)
 
@@ -102,6 +102,22 @@ class AlbumMemberInline(admin.TabularInline):
 
     extra = 0
     max_num = 0
+
+    def album_link(self, obj):
+        return format_html('<a href="{0}">{1}</a>',
+                '../../../{0}/{1}/{2}/'.format(obj.album._meta.app_label, obj.album._meta.module_name, obj.album.id),
+                obj.album.name)
+    album_link.short_description = 'Album'
+    album_link.admin_order_field = 'album'
+    album_link.allow_tags = True
+
+    def added_by_user_link(self, obj):
+        return format_html('<a href="{0}">{1}</a>',
+                '../../../{0}/{1}/{2}/'.format(obj.added_by_user._meta.app_label, obj.added_by_user._meta.module_name, obj.added_by_user.id),
+                str(obj.added_by_user))
+    added_by_user_link.short_description = 'Added by user'
+    added_by_user_link.admin_order_field = 'added_by_user'
+    added_by_user_link.allow_tags = True
 
 class PhoneContactInline(admin.TabularInline):
     model = PhoneContact
