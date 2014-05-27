d62dfeec8ad2ad51dc5901f2e0c1cde0a3e6bf83
benny <benny@shotvibe.com>
2014-05-27 03:07:44 +0300

    invites_manager: Add models and implementation and basic tests

--- invites_manager/models.py
+++ invites_manager/models.py
@@ -1,3 +1,111 @@
+import datetime
+import string
+
 from django.db import models
 
-# Create your models here.
+import phonenumbers
+
+from phone_auth.models import PhoneNumberLinkCode
+from photos.models import Album
+import phone_auth.sms_send
+
+
+class SMSInviteMessageManager(models.Manager):
+    def country_calling_code_use_default(self, country_calling_code):
+        if SMSInviteMessage.objects.filter(country_calling_code=country_calling_code, time_delay_hours=0).exists():
+            return False
+        else:
+            if not SMSInviteMessage.objects.filter(country_calling_code=None, time_delay_hours=0).exists():
+                raise RuntimeError('No default SMSInviteMessage exists in database')
+            return True
+
+    def invite_new_user(self, sms_invite_processor, link_code, current_time):
+        destination_phone_number = link_code.phone_number.phone_number
+        destination_country_calling_code = phonenumbers.parse(destination_phone_number).country_code
+
+        if SMSInviteMessage.objects.country_calling_code_use_default(destination_country_calling_code):
+            immediate_message_obj = SMSInviteMessage.objects.get(country_calling_code=None, time_delay_hours=0)
+            delayed_message_objs = SMSInviteMessage.objects.filter(country_calling_code=None, time_delay_hours__gt=0)
+        else:
+            immediate_message_obj = SMSInviteMessage.objects.get(country_calling_code=destination_country_calling_code, time_delay_hours=0)
+            delayed_message_objs = SMSInviteMessage.objects.filter(country_calling_code=destination_country_calling_code, time_delay_hours__gt=0)
+
+        for delayed_message in delayed_message_objs:
+            ScheduledSMSInviteMessage.objects.create(
+                    invite_sent_time = current_time,
+                    scheduled_delivery_time = current_time + datetime.timedelta(hours=delayed_message.time_delay_hours),
+                    link_code = link_code,
+                    message_template = delayed_message.message_template,
+                    time_delay_hours = delayed_message.time_delay_hours,
+                    sms_sender_phone_override = None)
+
+        sms_invite_processor.send_immediate_invite(link_code, immediate_message_obj.message_template, None)
+
+
+class SMSInviteMessage(models.Model):
+    country_calling_code = models.PositiveSmallIntegerField(null=True, blank=True, help_text=
+            'See <a href="http://en.wikipedia.org/wiki/List_of_country_calling_codes">Full List</a>. If blank then this will be the default')
+    message_template = models.TextField(help_text=
+            'Available variables: <code>${inviter} ${name} ${album}</code>')
+    time_delay_hours = models.PositiveIntegerField(help_text=
+            'Numbers of hours to wait after initial invitation before sending this message')
+
+    class Meta:
+        unique_together = ('country_calling_code', 'time_delay_hours')
+
+    objects = SMSInviteMessageManager()
+
+
+class ScheduledSMSInviteMessageManager(models.Manager):
+    def get_scheduled_till(self, time):
+        return ScheduledSMSInviteMessage.objects.filter(scheduled_delivery_time__lte=time)
+
+
+class ScheduledSMSInviteMessage(models.Model):
+    invite_sent_time = models.DateTimeField()
+    scheduled_delivery_time = models.DateTimeField()
+    link_code = models.ForeignKey(PhoneNumberLinkCode)
+    message_template = models.TextField()
+    time_delay_hours = models.PositiveIntegerField()
+    sms_sender_phone_override = models.CharField(max_length=32, null=True, blank=True)
+
+    objects = ScheduledSMSInviteMessageManager()
+
+
+class SMSInviteProcessor(object):
+    def __init__(self, sms_sender=phone_auth.sms_send.send_sms):
+        """
+        sms_sender must be a callable with the same signature as phone_auth.sms_send.send_sms
+        """
+        self.sms_sender = sms_sender
+
+    def send_immediate_invite(self, link_code, message_template, sms_sender_phone_override):
+        destination_phone = link_code.phone_number.phone_number
+
+        if sms_sender_phone_override:
+            sender_phone = sms_sender_phone_override
+        else:
+            inviter_phone = link_code.inviting_user.get_primary_phone_number()
+            if inviter_phone:
+                sender_phone = inviter_phone.phone_number
+            else:
+                sender_phone = None
+
+        # For showing the album, just grab the first album that the user belongs to
+        album = Album.objects.get_user_albums(link_code.phone_number.user.id)[0]
+
+        message = string.Template(message_template).safe_substitute(
+                inviter = link_code.inviting_user.nickname,
+                name = link_code.phone_number.user.nickname,
+                album = album)
+
+        invite_url_prefix = 'https://useglance.com'
+        link = link_code.get_invite_page(invite_url_prefix)
+
+        final_sms_message = message + '\n' + link
+        self.sms_sender(destination_phone, final_sms_message, sender_phone)
+
+    def process_scheduled_invites(self, current_time):
+        for scheduled_message in ScheduledSMSInviteMessage.objects.get_scheduled_till(current_time):
+            self.send_immediate_invite(scheduled_message.link_code, scheduled_message.message_template, scheduled_message.sms_sender_phone_override)
+            scheduled_message.delete()
--- invites_manager/tests.py
+++ invites_manager/tests.py
@@ -1,3 +1,146 @@
+import datetime
+
+from django.utils.timezone import utc
 from django.test import TestCase
 
-# Create your tests here.
+from phone_auth.models import User, PhoneNumber, PhoneNumberLinkCode
+from photos.models import Album, AlbumMember
+
+from invites_manager.models import SMSInviteProcessor, SMSInviteMessage
+
+
+class PretendSMSSender(object):
+    def __init__(self):
+        self.clear_sentbox()
+
+    def __call__(self, destination_phone, message, sender_phone=None):
+        self.sentbox.append((destination_phone, message, sender_phone))
+
+    def clear_sentbox(self):
+        self.sentbox = []
+
+
+class InviteTest(TestCase):
+    def setUp(self):
+        self.pretend_sms_sender = PretendSMSSender()
+        self.sms_invite_processor = SMSInviteProcessor(self.pretend_sms_sender)
+
+        self.amanda = User.objects.create_user('amanda')
+        PhoneNumber.objects.create(
+                phone_number = '+12127182002',
+                user = self.amanda,
+                date_created = datetime.datetime(1999, 01, 01, tzinfo=utc),
+                verified = True)
+
+        self.party_album = Album.objects.create_album(self.amanda, 'Party', datetime.datetime(2000, 01, 01, tzinfo=utc))
+
+    def test_single_default_invite(self):
+        SMSInviteMessage.objects.create(
+                country_calling_code = None, # Use as default
+                message_template = 'Hi ${name}. ${inviter} shared an album: ${album}',
+                time_delay_hours = 0)
+
+        the_time = datetime.datetime(2000, 01, 02, tzinfo=utc)
+
+        new_user = User.objects.create_user(nickname='barney')
+        phone_number = PhoneNumber.objects.create(
+                phone_number = '+12127182003',
+                user = new_user,
+                date_created = the_time,
+                verified = False)
+
+        link_code = PhoneNumberLinkCode.objects.create(
+                phone_number = phone_number,
+                invite_code = PhoneNumberLinkCode.generate_invite_code(),
+                inviting_user = self.amanda,
+                date_created = the_time,
+                was_visited = False)
+
+        AlbumMember.objects.create(
+                user = new_user,
+                album = self.party_album,
+                added_by_user = self.amanda,
+                datetime_added = the_time)
+
+        SMSInviteMessage.objects.invite_new_user(self.sms_invite_processor, link_code, the_time)
+
+        invite_url_prefix = 'https://useglance.com'
+        link = link_code.get_invite_page(invite_url_prefix)
+
+        self.assertEqual(self.pretend_sms_sender.sentbox, [('+12127182003', u'Hi barney. amanda shared an album: Party\n' + link, '+12127182002')])
+
+    def test_scheduled_default_invites(self):
+        SMSInviteMessage.objects.create(
+                country_calling_code = None, # Use as default
+                message_template = 'Hi ${name}. ${inviter} shared an album: ${album}',
+                time_delay_hours = 0)
+
+        SMSInviteMessage.objects.create(
+                country_calling_code = None, # Use as default
+                message_template = 'Hi ${name}. ${inviter} has been waiting 2 hours for you to view: ${album}',
+                time_delay_hours = 2)
+
+        SMSInviteMessage.objects.create(
+                country_calling_code = None, # Use as default
+                message_template = 'An entire day has passed',
+                time_delay_hours = 24)
+
+        the_time = datetime.datetime(2000, 01, 02, tzinfo=utc)
+
+        new_user = User.objects.create_user(nickname='barney')
+        phone_number = PhoneNumber.objects.create(
+                phone_number = '+12127182003',
+                user = new_user,
+                date_created = the_time,
+                verified = False)
+
+        link_code = PhoneNumberLinkCode.objects.create(
+                phone_number = phone_number,
+                invite_code = PhoneNumberLinkCode.generate_invite_code(),
+                inviting_user = self.amanda,
+                date_created = the_time,
+                was_visited = False)
+
+        AlbumMember.objects.create(
+                user = new_user,
+                album = self.party_album,
+                added_by_user = self.amanda,
+                datetime_added = the_time)
+
+        SMSInviteMessage.objects.invite_new_user(self.sms_invite_processor, link_code, the_time)
+
+        invite_url_prefix = 'https://useglance.com'
+        link = link_code.get_invite_page(invite_url_prefix)
+
+        self.assertEqual(self.pretend_sms_sender.sentbox, [('+12127182003', u'Hi barney. amanda shared an album: Party\n' + link, '+12127182002')])
+        self.pretend_sms_sender.clear_sentbox()
+
+        self.sms_invite_processor.process_scheduled_invites(datetime.datetime(2000, 01, 02, 0, 0, 0, tzinfo=utc))
+        self.assertEqual(self.pretend_sms_sender.sentbox, [])
+
+        self.sms_invite_processor.process_scheduled_invites(datetime.datetime(2000, 01, 02, 1, 0, 0, tzinfo=utc))
+        self.assertEqual(self.pretend_sms_sender.sentbox, [])
+
+        self.sms_invite_processor.process_scheduled_invites(datetime.datetime(2000, 01, 02, 2, 0, 0, tzinfo=utc))
+        self.assertEqual(self.pretend_sms_sender.sentbox, [(u'+12127182003', u'Hi barney. amanda has been waiting 2 hours for you to view: Party\n' + link, '+12127182002')])
+        self.pretend_sms_sender.clear_sentbox()
+
+        self.sms_invite_processor.process_scheduled_invites(datetime.datetime(2000, 01, 02, 3, 0, 0, tzinfo=utc))
+        self.assertEqual(self.pretend_sms_sender.sentbox, [])
+        self.pretend_sms_sender.clear_sentbox()
+
+        self.sms_invite_processor.process_scheduled_invites(datetime.datetime(2000, 01, 02, 23, 0, 0, tzinfo=utc))
+        self.assertEqual(self.pretend_sms_sender.sentbox, [])
+        self.pretend_sms_sender.clear_sentbox()
+
+        self.sms_invite_processor.process_scheduled_invites(datetime.datetime(2000, 01, 02, 23, 59, 0, tzinfo=utc))
+        self.assertEqual(self.pretend_sms_sender.sentbox, [])
+        self.pretend_sms_sender.clear_sentbox()
+
+        self.sms_invite_processor.process_scheduled_invites(datetime.datetime(2000, 01, 03, 2, 0, 0, tzinfo=utc))
+        self.assertEqual(self.pretend_sms_sender.sentbox, [(u'+12127182003', u'An entire day has passed\n' + link, u'+12127182002')])
+        self.pretend_sms_sender.clear_sentbox()
+
+        self.sms_invite_processor.process_scheduled_invites(datetime.datetime(2000, 01, 03, 2, 0, 1, tzinfo=utc))
+        self.assertEqual(self.pretend_sms_sender.sentbox, [])
+        self.pretend_sms_sender.clear_sentbox()
