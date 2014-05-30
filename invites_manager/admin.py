964e9af410d020dfea6fa99f482896aca352c751
benny <benny@shotvibe.com>
2014-05-30 18:08:09 +0300

    invites_manager: Setup Django admin for SMSInviteMessage

--- invites_manager/admin.py
+++ invites_manager/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from invites_manager.models import SMSInviteMessage
+
+
+class SMSInviteMessageAdmin(admin.ModelAdmin):
+    list_display = ('country_calling_code', 'time_delay_hours', 'message_template')
+    list_display_links = ('country_calling_code', 'time_delay_hours')
+
+    ordering = ('country_calling_code', 'time_delay_hours')
+
+
+admin.site.register(SMSInviteMessage, SMSInviteMessageAdmin)
