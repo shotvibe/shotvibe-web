bdf06cae70aefb35fc3744788b34fc6bc4e53c61
benny <benny@shotvibe.com>
2014-05-30 23:47:05 +0300

    invites_manager: Fix initial south migration by adding needed depends_on

--- invites_manager/migrations/0001_initial.py
+++ invites_manager/migrations/0001_initial.py
@@ -7,6 +7,10 @@ from django.db import models
 
 class Migration(SchemaMigration):
 
+    depends_on = (
+        ('phone_auth', '0004_auto__add_field_phonenumberlinkcode_was_visited'),
+    )
+
     def forwards(self, orm):
         # Adding model 'SMSInviteMessage'
         db.create_table(u'invites_manager_smsinvitemessage', (
