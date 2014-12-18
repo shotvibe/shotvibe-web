ad4e5007ee021b8ec04cb27aa5bab7075633ad4d
benny <benny@shotvibe.com>
2014-12-18 17:22:51 +0200

    Add to Django Admin 'PhotoComment' model

--- photos/admin.py
+++ photos/admin.py
@@ -5,7 +5,7 @@ from django.utils.html import format_html
 from django.utils.safestring import mark_safe
 
 from phone_auth.models import PhoneNumberLinkCode
-from photos.models import Album, AlbumMember, Photo, PendingPhoto, PhotoGlance
+from photos.models import Album, AlbumMember, Photo, PendingPhoto, PhotoComment, PhotoGlance
 from photos.models import PhotoServer
 
 class PhotoAdminInline(admin.TabularInline):
@@ -143,6 +143,35 @@ class PendingPhotoAdmin(admin.ModelAdmin):
     pass
 
 
+class PhotoCommentAdmin(admin.ModelAdmin):
+    list_display = ('photo_icon', 'album_link', 'comment_text', 'date_created', 'author_link')
+    list_display_links = list_display
+
+    ordering = ('-date_created',)
+
+    def photo_icon(self, obj):
+        return format_html(u'<img src="{0}" width="35" height="35">', obj.photo.get_photo_url_no_ext() + '_crop140.jpg')
+    photo_icon.short_description = 'Photo'
+    photo_icon.admin_order_field = 'photo'
+
+    def album_link(self, obj):
+        return format_html(u'<a href="{0}">{1}</a>',
+                u'../../{0}/{1}/{2}/'.format(obj.photo.album._meta.app_label, obj.photo.album._meta.module_name, obj.photo.album.id),
+                obj.photo.album.name)
+    album_link.short_description = 'Album'
+    album_link.admin_order_field = 'album'
+
+    def comment_text(self, obj):
+        return obj.comment_text
+
+    def author_link(self, obj):
+        return format_html(u'<a href="{0}">{1}</a>',
+                u'../../{0}/{1}/{2}/'.format(obj.author._meta.app_label, obj.author._meta.module_name, obj.author.id),
+                obj.author)
+    author_link.short_description = 'Author'
+    author_link.admin_order_field = 'author'
+
+
 class PhotoGlanceAdmin(admin.ModelAdmin):
     list_display = ('photo_icon', 'album_link', 'emoticon_icon', 'date_created', 'author_link')
     list_display_links = list_display
@@ -180,5 +209,6 @@ class PhotoServerAdmin(admin.ModelAdmin):
 admin.site.register(Photo, PhotoAdmin)
 admin.site.register(PendingPhoto, PendingPhotoAdmin)
 admin.site.register(Album, AlbumAdmin)
+admin.site.register(PhotoComment, PhotoCommentAdmin)
 admin.site.register(PhotoGlance, PhotoGlanceAdmin)
 admin.site.register(PhotoServer, PhotoServerAdmin)
