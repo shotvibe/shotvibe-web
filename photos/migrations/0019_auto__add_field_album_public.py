a987c319ab2f2e784084929cbc24df2b1f53bc67
benny <benny@shotvibe.com>
2015-11-21 22:01:42 +0000

    Add migration for `album_public' field

---
+++ photos/migrations/0019_auto__add_field_album_public.py
@@ -0,0 +1,153 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'Album.public'
+        db.add_column(u'photos_album', 'public',
+                      self.gf('django.db.models.fields.BooleanField')(default=False),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'Album.public'
+        db.delete_column(u'photos_album', 'public')
+
+
+    models = {
+        u'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        u'auth.permission': {
+            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'phone_auth.user': {
+            'Meta': {'object_name': 'User'},
+            'avatar_file': ('django.db.models.fields.CharField', [], {'default': "'s3:shotvibe-avatars-01:default-avatar-0021.jpg'", 'max_length': '128'}),
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Group']"}),
+            'id': ('django.db.models.fields.IntegerField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_registered': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'nickname': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'primary_email': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'+'", 'on_delete': 'models.SET_NULL', 'to': u"orm['phone_auth.UserEmail']", 'blank': 'True', 'null': 'True', 'db_index': 'False'}),
+            'user_glance_score': ('django.db.models.fields.IntegerField', [], {'default': '25'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Permission']"})
+        },
+        u'phone_auth.useremail': {
+            'Meta': {'object_name': 'UserEmail'},
+            'email': ('django.db.models.fields.EmailField', [], {'unique': 'True', 'max_length': '75'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['phone_auth.User']"})
+        },
+        u'photos.album': {
+            'Meta': {'object_name': 'Album'},
+            'creator': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'+'", 'to': u"orm['phone_auth.User']"}),
+            'date_created': ('django.db.models.fields.DateTimeField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'last_updated': ('django.db.models.fields.DateTimeField', [], {}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'public': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'revision_number': ('django.db.models.fields.IntegerField', [], {})
+        },
+        u'photos.albummember': {
+            'Meta': {'unique_together': "(('user', 'album'),)", 'object_name': 'AlbumMember', 'db_table': "'photos_album_members'"},
+            'added_by_user': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'created_album_memberships'", 'to': u"orm['phone_auth.User']"}),
+            'album': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'memberships'", 'to': u"orm['photos.Album']"}),
+            'album_admin': ('django.db.models.fields.BooleanField', [], {}),
+            'datetime_added': ('django.db.models.fields.DateTimeField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'last_access': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'album_membership'", 'to': u"orm['phone_auth.User']"})
+        },
+        u'photos.pendingphoto': {
+            'Meta': {'object_name': 'PendingPhoto'},
+            'author': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['phone_auth.User']"}),
+            'file_uploaded_time': ('django.db.models.fields.DateTimeField', [], {'null': 'True'}),
+            'photo_id': ('django.db.models.fields.CharField', [], {'max_length': '128', 'primary_key': 'True'}),
+            'processing_done_time': ('django.db.models.fields.DateTimeField', [], {'null': 'True'}),
+            'start_time': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'storage_id': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '128'})
+        },
+        u'photos.photo': {
+            'Meta': {'ordering': "['album_index']", 'unique_together': "(('album', 'album_index'),)", 'object_name': 'Photo'},
+            'album': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['photos.Album']"}),
+            'album_index': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
+            'author': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['phone_auth.User']"}),
+            'copied_from_photo': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['photos.Photo']", 'null': 'True', 'blank': 'True'}),
+            'date_created': ('django.db.models.fields.DateTimeField', [], {'db_index': 'True'}),
+            'photo_glance_score': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            'photo_id': ('django.db.models.fields.CharField', [], {'max_length': '128', 'primary_key': 'True'}),
+            'storage_id': ('django.db.models.fields.CharField', [], {'max_length': '128', 'db_index': 'True'}),
+            'subdomain': ('django.db.models.fields.CharField', [], {'max_length': '64'})
+        },
+        u'photos.photocomment': {
+            'Meta': {'unique_together': "(('photo', 'author', 'client_msg_id'),)", 'object_name': 'PhotoComment'},
+            'author': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['phone_auth.User']"}),
+            'client_msg_id': ('django.db.models.fields.BigIntegerField', [], {'db_index': 'True'}),
+            'comment_text': ('django.db.models.fields.TextField', [], {}),
+            'date_created': ('django.db.models.fields.DateTimeField', [], {'db_index': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'photo': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['photos.Photo']"})
+        },
+        u'photos.photoglance': {
+            'Meta': {'unique_together': "(('photo', 'author'),)", 'object_name': 'PhotoGlance'},
+            'author': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['phone_auth.User']"}),
+            'date_created': ('django.db.models.fields.DateTimeField', [], {'db_index': 'True'}),
+            'emoticon_name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'photo': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['photos.Photo']"})
+        },
+        u'photos.photoglancescoredelta': {
+            'Meta': {'unique_together': "(('photo', 'author'),)", 'object_name': 'PhotoGlanceScoreDelta'},
+            'author': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['phone_auth.User']"}),
+            'date_created': ('django.db.models.fields.DateTimeField', [], {'db_index': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'photo': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['photos.Photo']"}),
+            'score_delta': ('django.db.models.fields.IntegerField', [], {})
+        },
+        u'photos.photoserver': {
+            'Meta': {'object_name': 'PhotoServer'},
+            'auth_key': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'date_registered': ('django.db.models.fields.DateTimeField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'photos_update_url': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'}),
+            'subdomain': ('django.db.models.fields.CharField', [], {'max_length': '64', 'db_index': 'True'}),
+            'unreachable': ('django.db.models.fields.BooleanField', [], {})
+        },
+        u'photos.photousertag': {
+            'Meta': {'unique_together': "(('photo', 'tagged_user'),)", 'object_name': 'PhotoUserTag'},
+            'author': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'+'", 'to': u"orm['phone_auth.User']"}),
+            'date_created': ('django.db.models.fields.DateTimeField', [], {'db_index': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'photo': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['photos.Photo']"}),
+            'tag_coord_x': ('django.db.models.fields.FloatField', [], {}),
+            'tag_coord_y': ('django.db.models.fields.FloatField', [], {}),
+            'tagged_user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['phone_auth.User']"})
+        }
+    }
+
+    complete_apps = ['photos']
