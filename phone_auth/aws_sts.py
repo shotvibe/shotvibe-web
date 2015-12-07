9dce80517b4233cfde419c6406859f1708e70eef
benny <benny@shotvibe.com>
2015-12-07 16:16:00 +0000

    phone_auth.aws_sts Add some comments

--- phone_auth/aws_sts.py
+++ phone_auth/aws_sts.py
@@ -3,9 +3,13 @@ from django.conf import settings
 import boto.sts
 import json
 
+# See:
+#   https://www.whitneyindustries.com/aws/2014/11/16/boto-plus-s3-plus-sts-tokens.html
 def get_s3_upload_token(user, duration=900):
     policy_to_grant = {'Statement': [{'Action': ['s3:PutObject'],
                                       'Effect': 'Allow',
+                                      # TODO For security, limit permissions to
+                                      # upload only to user subdirectory:
                                       'Resource': ['arn:aws:s3:::glance-uploads/*']}]}
     stsconn = boto.sts.connect_to_region('us-east-1',
                                     aws_access_key_id=settings.AWS_ACCESS_KEY,
