ed1fc281c8144cf35b0aa3848340e93724e224df
benny <benny@shotvibe.com>
2016-02-25 16:02:41 +0000

    Add view for public_feed

--- photos/public_feed.py
+++ photos/public_feed.py
@@ -1,7 +1,11 @@
+import datetime
+
 from photos.models import Photo
 
-PUBLIC_FEED_TIME_THRESHOLD = datetime.timedelta(days=3)
+#PUBLIC_FEED_TIME_THRESHOLD = datetime.timedelta(days=3)
+PUBLIC_FEED_TIME_THRESHOLD = datetime.timedelta(days=90)
 PUBLIC_FEED_SCORE_THRESHOLD = 1
+PUBLIC_FEED_NUM_PHOTOS = 60
 
 def compute_public_feed(now):
     original_photos = Photo.objects.filter(date_created__gte=now-PUBLIC_FEED_TIME_THRESHOLD, copied_from_photo__isnull=True)
@@ -26,9 +30,15 @@ def compute_public_feed(now):
         y = total_photo_score[photo.photo_id]
         z = (now - photo.date_created).total_seconds()
         # TODO:
-        return z
+        return y
 
     photos = [p for p in original_photos]
+
+    for p in photos:
+        p.num_copies = num_copies[p.photo_id]
+        p.total_photo_score = total_photo_score[p.photo_id]
+        p.age = (now - p.date_created).total_seconds()
+
     photos.sort(key=photo_points, reverse=True)
 
-    return photos
+    return photos[:PUBLIC_FEED_NUM_PHOTOS]
